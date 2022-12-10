b2482b337aa7d11942edff17b0864d98fe06d052
Podckast <113110381+Podckast@users.noreply.github.com>
2022-12-10 19:03:33 +0300

    New_architecture

---
+++ Main.py
@@ -0,0 +1,70 @@
+import speech_recognition
+import webbrowser
+from random import randint
+import datetime
+import pyaudio
+import wave
+import pymysql
+
+class Commands():
+
+    def lms(self):
+        webbrowser.open('https://smartedu.hse.ru/')
+    def stop(self, text):
+        if text == 'стоп':
+            print('стоп')
+
+    def workout(self, exercise):
+        task = exercise[randint(0, len(exercise) - 1)]
+        count = randint(10, 40)
+        print(task, 'количество: ', count)
+
+
+    def Death_Note(self):
+        webbrowser.open('https://m.wcostream.net/anime/death-note')
+
+    def data_and_time(self):
+        date_object = str(datetime.datetime.now())
+        lst = date_object.split(" ")
+        date = lst[0].split("-")
+        time = lst[1].split(":")
+        print("сегодня", date[2], date[1], date[0], "время", time[0], time[1])
+
+    def record_and_save_audio_file(self,number_of_records, CHUNK=1024, FORMAT=pyaudio.paInt16, CHANNELS=2, RATE=44100,
+                                   RECORD_SECONDS=5):
+        for k in range(number_of_records):
+            print("Введите название файла")
+            WAVE_OUTPUT_FILENAME = input()
+            WAVE_OUTPUT_FILENAME += ".wav"
+            p = pyaudio.PyAudio()
+            stream = p.open(format=FORMAT,
+                            channels=CHANNELS,
+                            rate=RATE,
+                            input=True,
+                            input_device_index=1,
+                            frames_per_buffer=CHUNK)
+            print("recording")
+            frames = []
+            for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+                data = stream.read(CHUNK)
+                frames.append(data)
+            print("done recording")
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+
+            wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(frames))
+            wf.close()
+    def data_base(self):
+        connection = pymysql.connect(
+            host='localhost',
+            user='Artem',
+            password='myCode',
+            database='',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
--- Voice_helper.py
+++ Voice_helper.py
@@ -1,19 +1,42 @@
 import speech_recognition
 import webbrowser
 from random import randint
+import datetime
+import pyaudio
+import wave
+from Main import Commands
+exercise = ['Отжимания','Подтягивания','Приседания','Бег','Пресс']
+
+tools = {
+}
+
 def dispatcher(record):
-    if record == 'открой дневник':
-        Commands.lms(self=record)
+    try:
+        if record == 'открой дневник':
+            Commands.lms(self=record)
+        elif record == 'выдай упражнения':
+            Commands.workout(self=record, exercise=exercise)
+        elif record == 'который час':
+            Commands.data_and_time(self=record)
+        elif record == 'открой тетрадь смерти':
+            Commands.Death_Note(self=record)
+        elif record == 'хочу записать файл':
+            print("Сколько файлов вы хотите записать?")
+            count = int(input())  # количество файло
+            Commands.record_and_save_audio_file(self=record, number_of_records=count)
 
-    else:
-        raise ValueError
+        else:
+            raise ValueError
 
-def record_and_recognize_audio(*args: tuple):
-    """
-    Запись и распознавание аудио
-    """
+    except ValueError:
+        print('нормально говори дебил')
+
+
+def listner():
+    microphone = tools['microphone']
+    recognizer = tools['recognizer']
+
     with microphone:
-        recognized_data = ""
 
         # регулирование уровня окружающего шума
         recognizer.adjust_for_ambient_noise(microphone, duration=2)
@@ -21,48 +44,73 @@ def record_and_recognize_audio(*args: tuple):
         try:
             print("Listening...")
             audio = recognizer.listen(microphone, 5, 5)
+            return audio
 
         except speech_recognition.WaitTimeoutError:
-            print("Can you check if your microphone is on, please?")
-            return
-
-        # использование online-распознавания через Google
-        try:
-            print("Started recognition...")
-            recognized_data = recognizer.recognize_google(audio, language="ru").lower()
-
-        except speech_recognition.UnknownValueError:
-            pass
-
-        # в случае проблем с доступом в Интернет происходит выброс ошибки
-        except speech_recognition.RequestError:
-            print("Check your Internet Connection, please")
-
-        return recognized_data
+            print('check your microphone')
 
 
-class Commands():
+def recognize(audio):
+    recognizer = tools['recognizer']
 
-    def lms(self):
-        webbrowser.open('https://smartedu.hse.ru/')
+    recognized_data = ""
+    # использование online-распознавания через Google
 
-    def stop(self, text):
-        if text == 'стоп':
-            print('стоп')
+    try:
+        print("Started recognition...")
+        recognized_data = recognizer.recognize_google(audio, language="ru").lower()
 
-    def workout(self, exercise):
-        task = exercise[randint(0, len(exercise)-1)]
-        print(task)
+    except speech_recognition.UnknownValueError:
+        pass
 
-if __name__ == "__main__":
+    # в случае проблем с доступом в Интернет происходит выброс ошибки
+    except speech_recognition.RequestError:
+        print("Check your Internet Connection, please")
 
-    # инициализация инструментов распознавания и ввода речи
+    return recognized_data
+
+
+def start():
     recognizer = speech_recognition.Recognizer()
     microphone = speech_recognition.Microphone()
 
-    while True:
-        # старт записи речи с последующим выводом распознанной речи
-        voice_input = record_and_recognize_audio()
-        print(voice_input)
+    tools['recognizer'] = recognizer
+    tools['microphone'] = microphone
 
-# git push -f - НЕ ДЕЛАТЬ!!!!!!!!!
+    audio = listner()
+    voice_input = recognize(audio=audio).lower()
+
+    if 'афанасий' in voice_input:
+        voice_input = voice_input.replace('афанасий ', '')
+
+        if len(voice_input) > 0:
+            dispatcher(voice_input)
+
+    if 'тесты' in voice_input:
+        ...
+
+
+#       тут будут тесты.
+
+
+def working():
+    if __name__ == "__main__":
+
+        # инициализация инструментов распознавания и ввода речи
+        while True:
+            # старт записи речи с последующим выводом распознанной речи
+            audio = listner()
+            voice_input = recognize(audio=audio)
+
+            print(voice_input, '-- финальная расшифровка')
+
+            try:
+                dispatcher(voice_input)
+
+            except ValueError:
+                print('Я не нашёл команду')
+
+
+if __name__ == "__main__":
+    while True:
+        start()
