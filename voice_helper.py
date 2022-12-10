692657a583f63238033d0a860c1d90841f6098e3
Raykov Mikhail <raykov0417@mail.ru>
2022-12-10 20:07:16 +0300

    function name has been added.

---
+++ voice_helper.py
@@ -0,0 +1,91 @@
+import speech_recognition
+from main import Commands
+import subprocess
+
+tools = {
+}
+
+
+def dispatcher(record):
+    try:
+        if record == 'открой lms':
+            Commands.lms(self=record)
+
+        if record == 'открой калькулятор':
+            subprocess.Popen('C:\\Windows\\System32\\calc.exe')
+
+        if record == 'выдай упражнения':
+            exercise = ['Отжимания', 'подтягивания']
+            Commands.workout(self=record, exercise=exercise)
+        if record == 'меня зовут миша' or record =='меня зовут артём'or record =='меня зовут матвей':
+            Commands.hello(name=record[11:])
+        else:
+            raise ValueError
+
+    except ValueError:
+        print('нормально говори дебил')
+
+
+def listner():
+    microphone = tools['microphone']
+    recognizer = tools['recognizer']
+
+    with microphone:
+
+        # регулирование уровня окружающего шума
+        recognizer.adjust_for_ambient_noise(microphone, duration=2)
+
+        try:
+            print("Listening...")
+            audio = recognizer.listen(microphone, 5, 5)
+            return audio
+
+        except speech_recognition.WaitTimeoutError:
+            print('check your microphone')
+
+
+def recognize(audio):
+    recognizer = tools['recognizer']
+
+    recognized_data = ""
+    # использование online-распознавания через Google
+
+    try:
+        print("Started recognition...")
+        recognized_data = recognizer.recognize_google(audio, language="ru").lower()
+
+    except speech_recognition.UnknownValueError:
+        pass
+
+    # в случае проблем с доступом в Интернет происходит выброс ошибки
+    except speech_recognition.RequestError:
+        print("Check your Internet Connection, please")
+
+    return recognized_data
+
+
+def start():
+    recognizer = speech_recognition.Recognizer()
+    microphone = speech_recognition.Microphone()
+
+    tools['recognizer'] = recognizer
+    tools['microphone'] = microphone
+
+    audio = listner()
+    voice_input = recognize(audio=audio).lower()
+
+    if 'афанасий' in voice_input:
+        voice_input = voice_input.replace('афанасий ', '')
+
+        if len(voice_input) > 0:
+            print('нашёл команды')
+            dispatcher(voice_input)
+
+    if 'тесты' in voice_input:
+        ...
+
+
+#       тут будут тесты.
+if __name__ == "__main__":
+    while True:
+        start()
