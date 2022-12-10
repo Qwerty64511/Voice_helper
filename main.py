9895e6b47af2d7939bfd8d485d485853d26e0006
Raykov Mikhail <raykov0417@mail.ru>
2022-12-10 22:02:49 +0300

    Smart find is added and some new functions.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import webbrowser
 from random import randint
+import subprocess
 
 
 class Commands():
@@ -10,17 +11,27 @@ class Commands():
     def stop(self, text):
         if text == 'стоп':
             print('стоп')
+    def calc():
+        # subprocess.Popen('C:\\Windows\\System32\\calc.exe')
+        subprocess.Popen('calc.exe')
 
     def workout(self, exercise):
         task = exercise[randint(0, len(exercise) - 1)]
         value = randint(1, 20)
 
         print(task, value)
-    def smart_find(self, string):
-        pass
 
+    def smart_find_app(name):
+        subprocess.Popen(name)
+
+    def smart_find_site(name):
+        webbrowser.open(f'https://{name}')
+    
     def hello(name):
         print(f'Привет {name}')
+
     def youtube():
-        webbrowser.open('https://www.youtube.com/')
+        webbrowser.open('https://youtube.com/')
+    def mail():
+        webbrowser.open('https://mail.yandex.ru/')
     
